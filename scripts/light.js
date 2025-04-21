59b668023a033a6a0e854489b098e6338e370752
pupxrl <142365738+pupxrl@users.noreply.github.com>
2025-04-21 04:39:17 +0100

    Hmmmmmmmmmmmm

--- scripts/light.js
+++ scripts/light.js
@@ -2,10 +2,10 @@ const themeToggle = document.getElementById("theme-toggle");
 const themeStyle = document.getElementById("theme-style");
 
 let currentTheme = localStorage.getItem("theme") || "light";
-themeStyle.href = `../styles/${currentTheme}.css`;
+themeStyle.href = `/styles/${currentTheme}.css`;
 
 themeToggle.addEventListener("click", () => {
   currentTheme = currentTheme === "light" ? "dark" : "light";
-  themeStyle.href = `../styles/${currentTheme}.css`;
+  themeStyle.href = `/styles/${currentTheme}.css`;
   localStorage.setItem("theme", currentTheme);
 });
